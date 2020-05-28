98dd71e261874b559d8b13b7165b0b909e97f1f1
kyugie <chawisa_gieza@hotmail.com>
2020-05-28 12:04:00 +0700

    config

--- run_yelp.sh
+++ run_yelp.sh
@@ -29,20 +29,20 @@ python -u gen_data_fin.py \
     --signature=${signature} \
     --pool_size=${pool_size} \
 
-#CUDA_VISIBLE_DEVICES=0 python -u run.py \
-#    --train_input_file=./data/${dataset_name}${signature}.train.tfrecord \
-#    --test_input_file=./data/${dataset_name}${signature}.test.tfrecord \
-#    --vocab_filename=./data/${dataset_name}${signature}.vocab \
-#    --user_history_filename=./data/${dataset_name}${signature}.his \
-#    --checkpointDir=${CKPT_DIR}/${dataset_name} \
-#    --signature=${signature}-${dim} \
-#    --do_train=False\
-#    --do_eval=True \
-#    --bert_config_file=./bert_train/bert_config_${dataset_name}_${dim}.json \
-#    --batch_size=${batch_size} \
-#    --max_seq_length=${max_seq_length} \
-#    --max_predictions_per_seq=${max_predictions_per_seq} \
-#    --num_train_steps=${num_train_steps} \
-#    --num_warmup_steps=100 \
-#    --learning_rate=1e-4
+CUDA_VISIBLE_DEVICES=0 python -u run.py \
+    --train_input_file=./data/${dataset_name}${signature}.train.tfrecord \
+    --test_input_file=./data/${dataset_name}${signature}.test.tfrecord \
+    --vocab_filename=./data/${dataset_name}${signature}.vocab \
+    --user_history_filename=./data/${dataset_name}${signature}.his \
+    --checkpointDir=${CKPT_DIR}/${dataset_name} \
+    --signature=${signature}-${dim} \
+    --do_train=False\
+    --do_eval=True \
+    --bert_config_file=./bert_train/bert_config_${dataset_name}_${dim}.json \
+    --batch_size=${batch_size} \
+    --max_seq_length=${max_seq_length} \
+    --max_predictions_per_seq=${max_predictions_per_seq} \
+    --num_train_steps=${num_train_steps} \
+    --num_warmup_steps=100 \
+    --learning_rate=1e-4
 
